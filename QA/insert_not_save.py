d87721a4054b8d519a32959d1839da86b075af20
Xiang Wang <ramwin@qq.com>
2016-07-25 14:40:13 +0800

    mongoengine

--- QA/insert_not_save.py
+++ QA/insert_not_save.py
@@ -10,7 +10,8 @@ class User(Document):
     
     def sayhi(self):
         print('Hello, my name is %s' % self.name)
-    def save(self):
+    @classmethod
+    def pre_save(cls, sender, document, **kwargs):
         print('saving user %s...' % self.name)
         super(User, self).save()
     def __str__(self):
@@ -18,12 +19,14 @@ class User(Document):
 
 User.objects.delete()
 John = User(name='John')
-John.sayhi()
-John.save()
+John.sayhi() # Hello, my name is John
+John.save() # nothing
 
 Ross = User(name='Ross')
-Ross.sayhi()
-User.objects.insert(Ross)
+Ross.sayhi() # Hello, my name is Ross
+User.objects.insert(Ross) # there is no `saving user Ross...`
 
 for i in User.objects.all():
     print(i)
+# John
+# Ross
